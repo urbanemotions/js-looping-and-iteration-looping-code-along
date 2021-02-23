6434829d53820f43e97bbf042e163c679c85e2aa
sin wan wildy <sw.wildy@gmail.com>
2021-02-22 20:43:15 -0800

    completed

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Code your solutions in this file
+function writeCards(name, event)
+{
+    let results = [];
+
+    for (let i = 0; i < name.length; i++)
+    {           
+        results.push(`Thank you, ${name[i]}, for the wonderful ${event} gift!`);
+    }
+    return results;
+}
+
+
+function countDown()
+{
+    let count = 10;
+    while (count >= 0)
+    {
+        console.log(count);
+        count-=1 
+    }
+}
